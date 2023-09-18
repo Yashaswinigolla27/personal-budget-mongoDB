cd66c30c9beac05b2ab7dc0f2eb95db6c2ae429d
Yashaswini Golla <yashaswinigolla@Yashaswinis-MacBook-Air.local>
2023-09-17 21:12:19 -0400

    Added D3JS chart

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = 3030;
 
 app.use('/', express.static('public'));
 
+<<<<<<< HEAD
 /**
  * const budget = {
     myBudget: [
@@ -22,25 +23,34 @@ app.use('/', express.static('public'));
     ]
 };
 **/
+=======
+>>>>>>> 6f7fcca (Added D3JS chart)
 
 const fs = require('fs');
 // Read the JSON file
 const jsonData = fs.readFileSync('budget.json', 'utf8');
 // Parse the JSON data
+<<<<<<< HEAD
 const budget = JSON.parse(jsonData);
 // Access the budget data using the 'budget' variable
 console.log(budget);
+=======
+const budgetData = JSON.parse(jsonData);
+// Access the budget data using the 'budget' variable
+console.log(budgetData);
+
+>>>>>>> 6f7fcca (Added D3JS chart)
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.json(budgetData);
 });
-
 
 
 app.listen(port, () => {
     console.log(`serving at http://localhost:${port}`)
 });
+
